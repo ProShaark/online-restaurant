7dff7e064a7bff71e78ddaba95d6ff7e07ede0da
ProShaark <rohitinglee@yahoo.com>
2022-03-23 12:06:25 +0530

    This is the skeleton of online resturant Project.

---
+++ src/main/java/com/ProShaark/online/restaurant/OnlineRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.ProShaark.online.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineRestaurantApplication.class, args);
+	}
+
+}
